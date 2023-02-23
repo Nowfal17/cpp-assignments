11ffadef74db06a12e87921446933691867ce0f0
Nowfal <113840202+Nowfal17@users.noreply.github.com>
2023-02-23 22:37:27 +0530

    Add files via upload

---
+++ assignment 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int num1, num2;
+  cout << "Enter two integers: ";
+  cin >> number1;
+  cin >> number2;
+ 
+ if (number1 > number2) {
+    cout << number1 << " is greater than " << number2;
+  } 
+  else if (number2 > number1) {
+    cout << number2 << " is greater than " << number1;
+  }
+  else {
+    cout << "Both integers are equal";
+  }
+   return 0;
+}
